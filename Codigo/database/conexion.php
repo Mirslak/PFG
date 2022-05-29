1453fa3b4a16199825fa835299c4ccb1728a7d2a
Africa Olmedilla <93615345+Mirslak@users.noreply.github.com>
2022-05-29 17:24:03 +0200

    Delete conexion.php

--- Codigo/database/conexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-try{
-$conexion = mysqli_connect('192.168.1.169', 'debianDB', 'debianDB', 'Carbonicas');
-}catch(Exception $e){
-echo 'Ocurrió algo con la base de datos: ' . $e->getMessage();
-}
-?>
